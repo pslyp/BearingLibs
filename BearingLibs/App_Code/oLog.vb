9adf3994b1e892d237bbc24548494bab2e912560
PSlyp <nonny_038@hotmail.com>
2023-03-11 22:51:05 +0700

    Add project files.

---
+++ BearingLibs/App_Code/oLog.vb
@@ -0,0 +1,73 @@
+﻿Imports System.Globalization
+Imports System.IO
+
+Public Class oLog
+
+    Enum AppendMode
+        None = False
+        Append = True
+    End Enum
+
+    Private Shared _path As String = String.Empty
+    Private Shared _appendMode As Boolean = AppendMode.Append
+
+    Property Path As String
+        Set(value As String)
+            _path = value
+        End Set
+        Get
+            Return _path
+        End Get
+    End Property
+
+    Property Message As String
+
+    Sub New()
+
+    End Sub
+
+    Sub New(path As String)
+        _path = path
+    End Sub
+
+    Sub New(path As String, append As AppendMode)
+        _path = path
+        _appendMode = append
+    End Sub
+
+    Sub Save()
+        _write(_path, Message, _appendMode)
+    End Sub
+
+    Sub Info(message As String)
+        message = DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss", CultureInfo.GetCultureInfo("en-US")) & Space(1) & "[Info]:" & Space(1) & message
+        _write(_path, message, _appendMode)
+    End Sub
+
+    Sub Warning(message As String)
+        message = DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss", CultureInfo.GetCultureInfo("en-US")) & Space(1) & "[Warning]:" & Space(1) & message
+        _write(_path, message, _appendMode)
+    End Sub
+
+    Sub [Error](message As String)
+        message = DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss", CultureInfo.GetCultureInfo("en-US")) & Space(1) & "[Error]:" & Space(1) & message
+        _write(_path, message, _appendMode)
+    End Sub
+
+    Private Sub _write(path As String, msg As String, append As AppendMode)
+        Dim _sw As StreamWriter = Nothing
+        Try
+            _sw = New StreamWriter(path, append)
+            _sw.WriteLine(msg)
+        Catch ex As DirectoryNotFoundException
+
+        Catch ex As UnauthorizedAccessException
+
+        Catch ex As IOException
+
+        Finally
+            _sw.Close()
+        End Try
+    End Sub
+
+End Class
---
+++ BearingLibs/Default.aspx.vb
@@ -0,0 +1,23 @@
+﻿
+Imports System.IO
+Imports System.Net.Mail
+
+Partial Class _Default
+    Inherits System.Web.UI.Page
+
+    Dim logFile As String = "~/Logs/logs.log"
+    Dim log As oLog
+
+    Private Sub _Default_Load(sender As Object, e As EventArgs) Handles Me.Load
+        log = New oLog(Server.MapPath(logFile))
+    End Sub
+
+    Protected Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        log.Info("Test Log.")
+    End Sub
+
+    Protected Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        log.Error("Program has error.")
+    End Sub
+
+End Class
